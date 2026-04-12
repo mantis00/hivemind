d734dfd28e34985a640cc3c77324c3fe29e6352a
chausch-ISU <hauschildtcarter@gmail.com>
2026-04-12 17:28:38 -0500

    really resolve merge conflict this time

--- components/navigation/protected-nav-actions.tsx
+++ components/navigation/protected-nav-actions.tsx
@@ -42,7 +42,6 @@ export function ProtectedNavActions() {
 	return (
 		<div className='flex items-center justify-end gap-2 max-w-full'>
 			<QrScannerButton />
-			<NotificationDropdown />
 			<div className='pr-1'>
 				{' '}
 				{/* slight padding to prevent bell from being too close to buttons */}
