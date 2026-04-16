6809199bfcfd36f7a2e2e4cbd3033dfd02f12c49
nazarit19 <138739227+nazarit19@users.noreply.github.com>
2026-04-15 22:16:02 -0500

    changed to use orgid as folder name

--- components/enclosures/edit-species-org.tsx
+++ components/enclosures/edit-species-org.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useCallback, useState } from 'react'
-import { type OrgSpecies, useOrgSpeciesCareInstructions, useOrgDetails } from '@/lib/react-query/queries'
+import { type OrgSpecies, useOrgSpeciesCareInstructions } from '@/lib/react-query/queries'
 import {
 	useUpdateOrgSpecies,
 	useDeactivateOrgSpecies,
